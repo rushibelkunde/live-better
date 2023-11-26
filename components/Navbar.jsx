a23168feac2bd3756f98a33f101641423e98986f
rushibelkunde <rushibelkunde18@gmail.com>
2023-11-26 21:44:00 +0530

    media query

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -5,8 +5,8 @@ import Link from 'next/link'
 
 const Navbar = () => {
   return (
-    <header className=' bg-black w-full flex items-center justify-between px-5 h-16 text-white font-semibold'>
-        <Link className=' font-semibold text-2xl' href={"/"}>LiveBetter</Link>
+    <header className=' bg-black w-full flex items-center justify-between px-5 h-16 text-white font-semibold sm:text-base text-sm'>
+        <Link className=' font-semibold' href={"/"}>LiveBetter</Link>
         <Link href={"/medicines"}>medicines</Link>
         <Link href={"/appointments"}>appointments</Link>
         <Link href={"/emergency"}>emergency</Link>
